c681381c86d669639d33157e39e5a3ffa9c921a8
kihamala <kihamala@50eb543b-83fa-0310-9bb7-f7cbbee4ab39>
2009-03-23 11:43:07 +0000

    releasing
    git-svn-id: https://stage.maemo.org/svn/maemo/projects/haf/trunk/libmatchbox2@17772 50eb543b-83fa-0310-9bb7-f7cbbee4ab39

--- matchbox/comp-mgr/mb-wm-comp-mgr-clutter.c
+++ matchbox/comp-mgr/mb-wm-comp-mgr-clutter.c
@@ -212,7 +212,8 @@ mb_wm_comp_mgr_clutter_fetch_texture (MBWMCompMgrClient *client)
 
   if (mb_wm_util_untrap_x_errors () == BadDrawable)
     {
-      g_debug ("%s: BadDrawable for %lx", __FUNCTION__, client->wm_client->window);
+      g_debug ("%s: BadDrawable for %lx", __FUNCTION__,
+               client->wm_client->window);
       return;
     }
 
