56d1af6a6cbfc58d75106c1052205f3ab915683c
kihamala <kihamala@50eb543b-83fa-0310-9bb7-f7cbbee4ab39>
2009-06-04 13:45:41 +0000

    patch from Laszlo for NB#117853
    git-svn-id: https://stage.maemo.org/svn/maemo/projects/haf/trunk/libmatchbox2@18608 50eb543b-83fa-0310-9bb7-f7cbbee4ab39

--- matchbox/core/mb-wm-decor.c
+++ matchbox/core/mb-wm-decor.c
@@ -1162,7 +1162,16 @@ static void
 mb_wm_decor_button_destroy (MBWMObject* obj)
 {
   MBWMDecorButton * button = MB_WM_DECOR_BUTTON (obj);
-  MBWMMainContext * ctx = button->decor->parent_client->wmref->main_ctx;
+  MBWMMainContext * ctx = NULL;
+  
+  ctx = button &&
+	  button->decor &&
+	  button->decor->parent_client &&
+	  button->decor->parent_client->wmref ? 
+	  button->decor->parent_client->wmref->main_ctx : NULL;
+
+  if (!ctx)
+	  return;
   /*
    * We are doing the job in the mb_wm_decor_button_unrealize() while the
    * decoration still exists.
